9597643e541d93e493c23534b39b89e4fe036a77
Peter Dombovari <dombo.peter@gmail.com>
2020-06-30 13:31:33 +0200

    Add slider functionality

--- script.js
+++ script.js
@@ -1,6 +1,67 @@
-/*“ If you want to lay the best foundation possible I’d recommend taking this course. 
-  The depth the instructors go into is incredible. I now feel so confident about 
-  starting up as a professional developer. ”
+  let john = {
+    quote: "“ If you want to lay the best foundation possible I’d recommend taking this course." +
+    "The depth the instructors go into is incredible. I now feel so confident about " +
+    "starting up as a professional developer. ”",
+    name: "John Tarkpor",
+    profession: "Junior Front-end Developer",
+    photo: "images/image-john.jpg"
+  };
 
-  John Tarkpor
-  Junior Front-end Developer*/
+  let tanya = {
+    quote: "“ I’ve been interested in coding for a while but never taken the jump, until now." +
+          "I couldn’t recommend this course enough. I’m now in the job of my dreams and so " +
+          "excited about the future. ”",
+    name: "Tanya Sinclair",
+    profession: "UX Engineer",
+    photo: "images/image-tanya.jpg"
+  };
+
+  let people = [tanya, john];
+  let personNumber = 0;
+
+  let loadPerson = (personNumber) => {
+    let person = people[personNumber];
+
+    $(".quote").text(person.quote);
+    $(".name").text(person.name);
+    $(".profession").text(person.profession);
+    $(".portrait").attr("src", person.photo);
+  }
+
+  loadPerson(0);
+
+  $('button.next').on('click', function() {
+    nextPerson();
+    loadPerson(personNumber);
+  })
+
+  $('button.prev').on('click', function() {
+    previousPerson();
+    loadPerson(personNumber);
+  })
+
+  $(document).keydown(function(e) {
+    var code = e.keyCode;
+    if (code === 39) {
+      nextPerson();
+    } else if (code === 37) {
+      previousPerson();
+    }
+    loadPerson(personNumber);
+  })
+
+  let nextPerson = function() {
+    if (personNumber < people.length - 1) {
+      personNumber++;
+    } else if (personNumber === people.length - 1) {
+      personNumber = 0;
+    }
+  }
+
+  let previousPerson = function() {
+    if (personNumber > 0) {
+      personNumber--;
+    } else if (personNumber === 0) {
+      personNumber = people.length - 1;
+    }
+  }
